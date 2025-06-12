8990cd2b963e19e8ded1bc527ab3fa8f1e793226
creator2104 <vinitr.prajapati2104@gmail.com>
2025-06-13 05:05:00 +0530

    ADDED A COMMENT

--- src/Utils/Store.jsx
+++ src/Utils/Store.jsx
@@ -12,3 +12,6 @@ const store = configureStore({
 export default store 
 
 // after creating you store and including inside app check in extensions of browser (redux dev tools) that the store is working or not
+
+// 🔧 Why use LRU Cache?
+// In your YouTube clone, suppose a user searches for something they've already searched before — you can avoid hitting the API again and show cached results. But since memory is limited, you don’t want to keep ev/erything forever. That’s where LRU helps — it evicts the least recently used data when the cache limit is hit.
